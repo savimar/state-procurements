6d5e38b68e2a3c578356f5e56bd16e9f9f5b6a70
savimar <sam409yandex.ru>
2019-04-23 23:59:11 +0300

    Add project files.

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,172 @@
+﻿namespace Zakupki
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.dateTimePicker2 = new System.Windows.Forms.DateTimePicker();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(161, 69);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePicker1.TabIndex = 0;
+            this.dateTimePicker1.ValueChanged += new System.EventHandler(this.dateTimePicker1_ValueChanged);
+            // 
+            // dateTimePicker2
+            // 
+            this.dateTimePicker2.Location = new System.Drawing.Point(423, 69);
+            this.dateTimePicker2.Name = "dateTimePicker2";
+            this.dateTimePicker2.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePicker2.TabIndex = 1;
+            this.dateTimePicker2.ValueChanged += new System.EventHandler(this.dateTimePicker2_ValueChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(328, 332);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Начать";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(94, 76);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(42, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "С даты";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(382, 75);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(21, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "По";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(100, 170);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(165, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "путь к папке выходных файлов";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(277, 170);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(346, 20);
+            this.textBox1.TabIndex = 5;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(638, 170);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(99, 23);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "выбор папки";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(369, 253);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(0, 13);
+            this.label4.TabIndex = 8;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(325, 265);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(0, 13);
+            this.label5.TabIndex = 9;
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 380);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dateTimePicker2);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Name = "Form1";
+            this.Text = "Закупки";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,275 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.ServiceModel.Syndication;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace Zakupki
+{
+    public partial class Form1 : Form
+    {
+        private DateTime DateFrom { get; set; }
+        private DateTime DateTo { get; set; }
+        private string FolderName { get; set; }
+        private HashSet<string> Errors { get; set; }
+
+        public Form1()
+        {
+            InitializeComponent();
+            dateTimePicker1.Format = DateTimePickerFormat.Short;
+            dateTimePicker2.Format = DateTimePickerFormat.Short;
+            dateTimePicker1.Value = DateTime.Now.AddDays(-4);
+            dateTimePicker2.Value = DateTime.Now.AddDays(-1);
+            dateTimePicker2.MaxDate = DateTime.Now.AddDays(-1);
+            label5.Text = "Статус: не начато";
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+        {
+            DateFrom = dateTimePicker1.Value.Date;
+        }
+
+        private void dateTimePicker2_ValueChanged(object sender, EventArgs e)
+        {
+            DateTo = dateTimePicker2.Value.AddDays(1).Date.AddMilliseconds(-1);
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            DialogResult result = folderBrowserDialog1.ShowDialog();
+
+            if (result == DialogResult.OK)
+            {
+                FolderName = folderBrowserDialog1.SelectedPath;
+                textBox1.Text = FolderName;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label5.Text = "Статус: Загрузка";
+            GetOrders();
+            label5.Text = "Статус: Окончено";
+        }
+
+        private void GetOrders()
+        {
+            List<string> dates = new List<string>();
+
+            foreach (DateTime day in EachDay(DateFrom, DateTo))
+            {
+                dates.Add(day.ToString("yyyyMMdd"));
+            }
+
+            //223 fz
+
+            string url223 = "ftp://ftp.zakupki.gov.ru/out/published/";
+            string password223 = "fz223free";
+
+            FtpWebRequest request223 = CreateRequest(url223,
+                WebRequestMethods.Ftp.ListDirectory, password223);
+
+            List<string> listRegion223 = GetResponseList(request223);
+
+
+            foreach (string region in listRegion223)
+            {
+                try
+                {
+                    string url = url223 + region + "/purchaseNotice/daily/";
+
+                    if (!(region.Equals("archive") || region.EndsWith(".txt") ||
+                          region.EndsWith(".sh") || region.Equals("undefined")))
+                    {
+                        GetFilesFromRegion(url, password223, dates);
+                    }
+                }
+                catch (Exception e)
+                {
+                    MessageBox.Show("по 223 фз ошибка: " + e);
+                }
+            }
+
+
+            //94 fz
+
+            string url94 = "ftp://ftp.zakupki.gov.ru/fcs_regions/";
+            string password94 = "free";
+
+            FtpWebRequest request94 = CreateRequest(url94,
+                WebRequestMethods.Ftp.ListDirectory, password94);
+
+            List<string> listRegion94 = GetResponseList(request94);
+
+            foreach (var region in listRegion94)
+                try
+                {
+                    string url = url94 + region + "/notifications/currMonth/";
+
+                    if (!(region.Equals("_logs") || region.Equals("PG-PZ") || region.EndsWith(".zip") ||
+                          region.Equals("control99docs") || region.Equals("fcs_undefined") || region.EndsWith(".sh")
+                          || region.EndsWith(".txt") || region.EndsWith(".log")))
+                    {
+                        GetFilesFromRegion(url, password94, dates);
+                    }
+                }
+                catch (Exception e)
+                {
+                    MessageBox.Show("по 94 фз ошибка: " + e);
+                }
+        }
+
+        private void GetFilesFromRegion(string url, string password, List<string> dates)
+        {
+            FtpWebRequest requestRegion =
+                CreateRequest(url, WebRequestMethods.Ftp.ListDirectory, password);
+
+            var allFiles = GetResponseList(requestRegion);
+
+            HashSet<string> files = new HashSet<string>();
+
+            Errors = new HashSet<string>();
+
+
+            foreach (string file in allFiles)
+            {
+                foreach (var date in dates)
+                {
+                    if (file.Contains(date))
+                    {
+                        files.Add(file);
+                    }
+                }
+            }
+
+            foreach (var file in files)
+            {
+                try
+                {
+                    Download(url + file, file, FolderName, password);
+                }
+                catch (Exception e)
+                {
+                    Errors.Add(file);
+                }
+            }
+
+            if (Errors.Count > 0)
+            {
+                foreach (var file in Errors)
+                {
+                    try
+                    {
+                        Download(url + file, file, FolderName, password);
+                    }
+                    catch (Exception e)
+                    {
+                        MessageBox.Show("Файл " + url + file + " не  удалось скачать с ftp сервера");
+                    }
+                }
+            }
+        }
+
+
+        private FtpWebRequest CreateRequest(string uri, string method, string credentials)
+        {
+            FtpWebRequest request = (FtpWebRequest) WebRequest.Create(uri);
+            request.Method = method;
+            request.Credentials = new NetworkCredential(credentials, credentials);
+
+            return request;
+        }
+
+
+        private List<string> GetResponseList(FtpWebRequest request)
+        {
+            var list = new List<string>();
+
+            using (var response = (FtpWebResponse) request.GetResponse())
+            {
+                using (var stream = response.GetResponseStream())
+                {
+                    using (var reader = new StreamReader(stream, true))
+                    {
+                        while (!reader.EndOfStream)
+                        {
+                            list.Add(reader.ReadLine());
+                        }
+                    }
+                }
+            }
+
+            return list;
+        }
+
+
+        private IEnumerable<DateTime> EachDay(DateTime from, DateTime to)
+        {
+            for (var day = from.Date; day.Date <= to.Date; day = day.AddDays(1))
+                yield return day;
+        }
+        
+
+        public void Download(string remotePath, string fileNameToDownload, string saveToLocalPath, string password)
+        {
+            try
+            {
+                FtpWebRequest request = CreateRequest(remotePath, WebRequestMethods.Ftp.DownloadFile, password);
+                request.UseBinary = true;
+                request.KeepAlive = false;
+
+                FtpWebResponse response = request.GetResponse() as FtpWebResponse;
+                Stream responseStream = response.GetResponseStream();
+                FileStream outputStream = new FileStream(saveToLocalPath + "\\" + fileNameToDownload, FileMode.Create);
+
+                int bufferSize = 1024;
+                int readCount;
+                byte[] buffer = new byte[bufferSize];
+
+                readCount = responseStream.Read(buffer, 0, bufferSize);
+                while (readCount > 0)
+                {
+                    outputStream.Write(buffer, 0, readCount);
+                    readCount = responseStream.Read(buffer, 0, bufferSize);
+                }
+
+                responseStream.Close();
+                outputStream.Close();
+                response.Close();
+            }
+            catch (Exception e)
+            {
+                Errors.Add(remotePath);
+                MessageBox.Show("Файл " + remotePath + " не скачался с ftp сервера");
+            }
+        }
+    }
+}
